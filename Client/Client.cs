42b6e99cf865fd546f6f99c0b08698854dd107c9
Artem <Artem@DESKTOP-99O26RC>
2023-02-19 10:17:28 +0100

    Добавьте файлы проекта.

---
+++ Client/Client.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpClient client = new TcpClient("localhost", 8080);
+                Console.WriteLine("Connected to server.");
+
+                NetworkStream stream = client.GetStream();
+
+                Console.WriteLine("Enter two currency codes (e.g. USD EUR): ");
+                string message = Console.ReadLine();
+
+                byte[] request = System.Text.Encoding.ASCII.GetBytes(message);
+                stream.Write(request, 0, request.Length);
+
+                byte[] response = new byte[256];
+                int bytesRead = stream.Read(response, 0, response.Length);
+                string rate = System.Text.Encoding.ASCII.GetString(response, 0, bytesRead);
+
+                Console.WriteLine("Exchange rate: " + rate);
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Currenciesexchanger/Server.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new CurrencyExchangeServer();
+            server.Start();
+
+            Console.WriteLine("Server started, press any key to stop...");
+            Console.ReadKey();
+
+            server.Stop();
+
+        }
+    }
+}
+
+public class CurrencyExchangeServer
+{
+    private TcpListener listener;
+    private ConcurrentDictionary<string, TcpClient> clients = new ConcurrentDictionary<string, TcpClient>();
+    public void Start()
+    {
+        listener = new TcpListener(IPAddress.Any, 8080);
+        listener.Start();
+        Task.Run(async () =>
+        {
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                string id = Guid.NewGuid().ToString();
+                clients.TryAdd(id, client);
+                Console.WriteLine($"Client{id} connected at {DateTime.Now}");
+                Task.Run(() => HandleClientAsync(id, client));
+            }
+        });
+    }
+
+    public void Stop()
+    {
+        listener.Stop();
+        foreach (var client in clients.Values)
+        {
+            client.Close();
+        }
+        Console.WriteLine("Server stopped");
+    }
+
+    private async Task HandleClientAsync(string id, TcpClient client)
+    {
+        var stream = client.GetStream();
+        while(true)
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+            if(bytesRead == 0)
+            {
+                break;
+            }
+
+            string message = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            string[] currencies = message.Split(' ');
+            if(currencies.Length == 2)
+            {
+                string from = currencies[0];
+                string to = currencies[1];
+                double rate = GetExchangeRate(from, to);
+                string response = rate.ToString();
+                byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseBytes, 0, responseBytes.Length);
+                Console.WriteLine($"Exchange rate from {from} to {to} sent to client {id}: {rate}");
+            }
+            else
+            {
+                Console.WriteLine($"Invalid message received from client {id}: {message}");
+            }
+        }
+
+        client.Close();
+        TcpClient removedClient;
+        clients.TryRemove(id, out removedClient );
+        Console.WriteLine($"Client{id} disconnected at {DateTime.Now}");
+    }
+
+    private double GetExchangeRate(string from, string to) 
+    {
+        return 1.2;
+    }
+}
+
